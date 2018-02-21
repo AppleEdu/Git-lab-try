52b3be5f4fb90eb45e5e083a12cd0e2dc2661fd0
Apple <appleeducation2018@taylors-macbook-pro-m15.local>
2018-02-21 08:36:09 -0700

    South Park

--- Git-lab-try/AppDelegate.swift
+++ Git-lab-try/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        NSLog("screw you guys, i'm going home!");
         return true
     }
 
